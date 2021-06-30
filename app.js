9d97599c1e2094c8ff036d1324d6a6674bfaee86
Jonathan Wright <j.wright72127@gmail.com>
2021-06-30 13:41:45 -0600

    Added Main Functionality!

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('Hey There!')
+
+function play(playerChoice) {
+    let cpChoice = 'scissors'
+
+    if (cpChoice == playerChoice) {
+        console.log('Draw')
+    } else if (playerChoice == 'rock') {
+        if (cpChoice == 'scissors') {
+            console.log('WIN')
+        } else if (cpChoice == 'paper') {
+            console.log('LOSE')
+        }
+    } else if (playerChoice == 'paper') {
+        if (cpChoice == 'rock') {
+            console.log('WIN')
+        } else if (cpChoice == 'scissors') {
+            console.log('LOSE')
+        }
+    } else if (playerChoice == 'scissors') {
+        if (cpChoice == 'paper') {
+            console.log('WIN')
+        } else if (cpChoice == 'rock') {
+            console.log('LOSE')
+        }
+    }
+}
+
+function computorChoice() {
+
+}
